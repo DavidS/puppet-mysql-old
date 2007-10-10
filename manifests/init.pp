668c57be350db67a14c85555d4e8996b13b85c10
root <root@puppetmaster.black.co.at>
2007-10-10 12:10:49 +0200

    mysql: create new skeleton module

---
+++ manifests/init.pp
@@ -0,0 +1,21 @@
+# mysql.pp
+# Copyright (C) 2007 David Schmitt <david@schmitt.edv-bus.at>
+# See LICENSE for the full license granted to you.
+
+class mysql::server {
+
+	package { "mysql-server":
+		ensure => installed,
+	}
+
+	munin::plugin {
+		[mysql_bytes, mysql_queries, mysql_slowqueries, mysql_threads]:
+	}
+
+	service { mysql:
+		ensure => running,
+		pattern => "mysqld",
+		require => Package["mysql-server"],
+	}
+
+}
